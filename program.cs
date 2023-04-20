06078734ee9a4f3bae05c25a959592bac2ada44f
Denis <stychynskidenis@gmail.com>
2023-04-20 12:14:28 +0300

    Написали программу

---
+++ program.cs
@@ -0,0 +1,26 @@
+﻿string[] array1 = new string[5] {"342", "3", "hell", "world", "res"};
+string[] array2 = new string[array1.Length];
+void FirstArrayInSecondArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+        
+    }
+    Console.Write("]");
+}
+FirstArrayInSecondArray(array1, array2);
+PrintArray(array2);
